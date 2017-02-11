73c78b55ecd061fc15448790b82ddeee6c1da1e7
Jacinto Zaitzewsky <micales@hotmail.se>
2017-02-11 16:40:27 +0100

    Added methods for UserService with unittests for them.

---
+++ NetForum/Exceptions/Validation/ServerValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exceptions.Validation
+{
+    public class ServerValidationException : Exception
+    {
+        public ServerValidationException()
+        {
+
+        }
+
+        public ServerValidationException(string message) : base(message)
+        {
+
+        }
+
+        public ServerValidationException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
---
+++ NetForum/UserAccountService/Service/UserService.cs
@@ -0,0 +1,58 @@
+﻿using Domain.Interface;
+using Domain.Model;
+using Exceptions.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UoW.Interface;
+using UserAccountServiceNameSpace.Interface;
+
+namespace UserAccountServiceNameSpace.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = _unitOfWork.GetUserRepository();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            try
+            {
+                var users = await _userRepository.GetAllAsync();
+                if(users == null)
+                    throw new ServerValidationException("No users found.");
+                if (!users.Any())
+                    throw new ServerValidationException("No users found.");
+
+                return users;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<User> GetAsync(User user)
+        {
+            try
+            {
+                var asyncUser = await _userRepository.GetAsync(user);
+                if (asyncUser == null)
+                    throw new ServerValidationException("No users found.");
+
+                return asyncUser;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
