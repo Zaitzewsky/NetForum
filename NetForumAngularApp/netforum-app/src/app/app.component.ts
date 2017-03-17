11420b34a968c3517189481faa4e9eb3b2286a25
Jacinto Zaitzewsky <micales@hotmail.se>
2017-03-18 00:28:26 +0100

    Added quickstart app from guide on Angular.io

---
+++ NetForumAngularApp/netforum-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My name is Jacinto and I am';
+  age = 22;
+}
