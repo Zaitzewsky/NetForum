348a6eccd0549179765dd7c182ef9785f2899fb1
Jacinto Zaitzewsky <micales@hotmail.se>
2017-03-18 01:36:55 +0100

    Accidently forgot to branch. Pushing directly to master.

--- NetForumAngularApp/netforum-app/src/app/app.component.ts
+++ NetForumAngularApp/netforum-app/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+import { User }    from './models/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Huehue';
-  age = ':D';
+  title = 'User page';
+  user = new User('Jacinto', 'Micales', 'Jazz', 'Backflip180', '07396602569', 'micales@hotmail.se');
+  submitted = false;
+
+  onsubmit(){
+    this.submitted = true;
+  }
+
+  get diagnostic(){ 
+    return JSON.stringify(this.user); 
+  }
 }
