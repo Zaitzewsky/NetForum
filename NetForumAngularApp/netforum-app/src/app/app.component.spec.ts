8520f91176ff8177d8b340fc8b0c83bb4d1e2ab4
Jacinto Zaitzewsky <micales@hotmail.se>
2017-03-18 03:07:48 +0100

    Added RegisterForm (just UI part). Have not added unit tests.

--- NetForumAngularApp/netforum-app/src/app/app.component.spec.ts
+++ NetForumAngularApp/netforum-app/src/app/app.component.spec.ts
@@ -1,37 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
 
-import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/app.register.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        RegisterComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(RegisterComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'Huehue :D'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(RegisterComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title + ' ' + app.age).toEqual('Huehue :D');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(RegisterComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent);
   }));
 
     it('should render title in a p tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(RegisterComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent);
